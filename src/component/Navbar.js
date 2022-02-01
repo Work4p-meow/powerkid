88715c74a30854d30a1d4ffb29a630ad28d5da08
iolq <wipawee_watt@cmu.ac.th>
2022-02-02 00:10:14 +0700

    updated

---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+
+
+const Navbar = () => {
+    return (
+        <div className='my-5'>
+            <p className='text-center text-3xl italic my-5'>หน้าหลัก</p>
+
+
+        </div>
+    )
+}
+
+export default Navbar
