b81a19ce43ba07980643f5481b0f9dd8442a8059
iolq <wipawee_watt@cmu.ac.th>
2022-02-02 00:47:27 +0700

    updated

---
+++ src/page/Enroll.js
@@ -0,0 +1,7 @@
+import Navbar from '../component/Navbar';
+import Footer from '../component/Footer';
+function Enroll() {
+
+}
+
+export default Enroll
---
+++ src/page/Trial.js
@@ -0,0 +1,7 @@
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+function Trial() {
+
+}
+
+export default Trial
