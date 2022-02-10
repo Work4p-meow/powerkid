2e60e478b5c1cc4d744eb314aaff6f31671efe88
Fujiph <aueboonruanekamfu@gmail.com>
2022-02-10 23:14:02 +0700

    footer updated

--- src/page/Cost.js
+++ src/page/Cost.js
@@ -1,10 +1,11 @@
 import Navbar from "../component/Navbar"
-
+import Footer from "../component/Footer"
 function Cost() {
     return(
         <>
             <Navbar />
         cost
+            <Footer />
         </>
     )
     //ราคา
--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,11 @@
 import NavbarHome from "../component/NavbarHome";
-
+import Footer from "../component/Footer";
 function Home(){
     return(
         <>
             <NavbarHome />
         Home
+            <Footer />
         </>
     )
 }
